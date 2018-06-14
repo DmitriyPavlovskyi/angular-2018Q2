ac348f9ea8d6cc92ffa4eca60627d3b846be73b7
dmitriypavlovskyi <pavlovskyidmitriy@gmail.com>
2018-06-14 12:23:18 +0300

    Basic router and home page added

--- todo-app/src/app/app.module.ts
+++ todo-app/src/app/app.module.ts
@@ -4,6 +4,9 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { TodoListModule } from './todo-list/todo-list.module';
+import { RouterModule } from '@angular/router'
+import { ROUTES } from './app.routes';
+import { HomeModule } from './home/home.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +15,9 @@ import { TodoListModule } from './todo-list/todo-list.module';
   imports: [
     BrowserModule,
     CoreModule,
-    TodoListModule
+    TodoListModule,
+    HomeModule,
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
