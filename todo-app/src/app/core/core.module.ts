eef42ccb69fe7195c56d758f69168197b71d218f
dmitriypavlovskyi <pavlovskyidmitriy@gmail.com>
2018-06-14 14:20:17 +0300

    breadcrumbs and toolbox added

--- todo-app/src/app/core/core.module.ts
+++ todo-app/src/app/core/core.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
+import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
+import { ToolboxComponent } from './toolbox/toolbox.component';
 
 @NgModule({
   imports: [
     CommonModule,
   ],
-  declarations: [HeaderComponent],
+  declarations: [HeaderComponent, BreadcrumbsComponent, ToolboxComponent],
   exports: [HeaderComponent]
 })
 export class CoreModule { }
