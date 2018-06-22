ba86ed8720b31e62dc2a0a2692cf5f9db67c618f
unknown <pavlovskyidmitriy@gmail.com>
2018-06-22 11:57:46 +0300

    Header and footer moved

--- src/app/core/home/home.module.ts
+++ src/app/core/home/home.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './component/home.component';
 import { RouterModule } from '@angular/router';
+import { FeaturesModule } from '@app/features/features.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    FeaturesModule
   ],
   declarations: [HomeComponent]
 })
