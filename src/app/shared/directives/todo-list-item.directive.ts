8fa59cf7508cc544efe2bb81d18d5db4377c934c
unknown <pavlovskyidmitriy@gmail.com>
2018-07-06 13:02:51 +0300

    Date comparison changed

--- src/app/shared/directives/todo-list-item.directive.ts
+++ src/app/shared/directives/todo-list-item.directive.ts
@@ -4,36 +4,20 @@ import { Directive, Renderer2, ElementRef, OnInit, Input } from '@angular/core';
   selector: '[appTodoHighlight]'
 })
 export class HighlightDirective implements OnInit {
-  @Input() courseDate: string;
+  @Input() courseDate: number;
   constructor(private el: ElementRef, private renderer: Renderer2) {}
 
   ngOnInit() {
-    console.log('course date');
     this.hightlightCourseFreshness(this.courseDate);
-
   }
 
-  private hightlightCourseFreshness(courseDate: string): void {
-    // let currentDate = new Date();
-    // let currentDay = currentDate.getDate();
-    // let currentMonth = currentDate.getMonth()+1; //January is 0!
-    // let currentYear = currentDate.getFullYear();
-    //
-    // if (currentDay < 10) {
-    //     currentDay = '0' + currentDay;
-    // }
-    // 
-    // if(currentMonth < 10) {
-    //     currentMonth = '0' + currentMonth
-    // }
-    //
-    // currentDate = currentDay + '/' + currentMonth + '/' + currentYear;
+  private hightlightCourseFreshness(courseDate: number): void {
+    let currentDate = new Date().getTime();
 
-
-    if (courseDate === '06.07.2018') {
+    if (courseDate > currentDate) {
+      this.renderer.setStyle(this.el.nativeElement, 'border', '1px solid blue');
+    } else if (courseDate <= currentDate){
       this.renderer.setStyle(this.el.nativeElement, 'border', '1px solid green');
-    } else if (courseDate === '21.06.2018'){
-      this.renderer.setStyle(this.el.nativeElement, 'border', '1px solid red');
     } else {
       this.renderer.setStyle(this.el.nativeElement, 'border', 'none');
     }
--- src/app/shared/models/todo-list-item.model.ts
+++ src/app/shared/models/todo-list-item.model.ts
@@ -2,7 +2,7 @@
 export interface TodoListItem {
   id: number;
   title: string;
-  creationDate: string;
+  creationDate: number;
   duration: number;
   description: string;
 }
